c8377d0ebf6814a91bbc99cf60737cfa00046748
Alex <skutsevol49@gmail.com>
2019-11-10 15:07:09 -0600

    adding some methods

--- src/firstClass.java
+++ src/firstClass.java
@@ -1,5 +1,15 @@
 public class firstClass {
     public static void main(String[] args) {
         System.out.println("Mahabat cannot print simple words ");
+
+        System.out.println("Norbert");
+
+        System.out.println("???");
+
+
+    }
+
+    void method () {
+
     }
 }
